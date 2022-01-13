b61d52d7b98a3c4daf80532e8cb49085d06b5656
Zafer Bozkurt <zbzkurt@gmail.com>
2022-01-13 12:54:29 +0300

    integration electrode

--- NobetciEczaneNative/ViewController.swift
+++ NobetciEczaneNative/ViewController.swift
@@ -6,14 +6,22 @@
 //
 
 import UIKit
+import ElectrodeContainer
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var rnView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        let viewController = ElectrodeReactNative.sharedInstance().miniApp(withName: "EczMiniapp", properties: nil, overlay: true, sizeFlexibility: 300, delegate: nil)
+        
+        viewController.view.frame = UIScreen.main.bounds;
+        self.rnView.addSubview(viewController.view)
     }
 
-
 }
+
 
